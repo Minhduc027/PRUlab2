71b888bdc159992a33ed214b8b6a78bb8f16a2dc
Minhduc027 <minhduylongthuan@gmail>
2024-07-10 22:44:29 +0700

    finishing

--- Assets/Scripts/MenuScoreDisplay.cs
+++ Assets/Scripts/MenuScoreDisplay.cs
@@ -11,10 +11,10 @@ public class MenuScoreDisplay : MonoBehaviour
     // Start is called before the first frame update
     private void OnEnable()
     {
-        score.text = String.Format("Score: ${0}", DataLoader.Instance.HighScore);
+        score.text = String.Format("Score: {0}", DataLoader.Instance.HighScore);
         int timeCount = DataLoader.Instance.TimeStamp;
         int minutes = Mathf.FloorToInt(timeCount / 60);
         int seconds = Mathf.FloorToInt(timeCount % 60);
-        time.text = String.Format("Max Time: {0:00}:{1:00}",minutes,seconds);
+        time.text = String.Format("Fastest Time: {0:00}:{1:00}",minutes,seconds);
     }
 }
